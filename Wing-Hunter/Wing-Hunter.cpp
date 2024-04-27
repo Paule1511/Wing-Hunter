5f22e8c93e538829c8a892cf569d3237d8c12de9
Paule1511 <astpauladolf@gmail.com>
2024-04-27 13:23:19 +0200

    Projektdateien hinzufügen.

---
+++ Wing-Hunter/Wing-Hunter.cpp
@@ -0,0 +1,12 @@
+﻿// Wing-Hunter.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
