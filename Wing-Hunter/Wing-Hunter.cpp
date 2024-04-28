efaca583e0bcfb864b2ea022cebb5c653a599c0c
Paule1511 <astpauladolf@gmail.com>
2024-04-28 13:29:42 +0200

    Base Repo Update No SDL2
    I HATE GIT

--- Wing-Hunter/Wing-Hunter.cpp
+++ Wing-Hunter/Wing-Hunter.cpp
@@ -2,22 +2,14 @@
 //
 
 #include <iostream>
-#include <SDL.h>
+#include "Wing-Utils.h"
 #undef main
 
 using namespace std;
 
 int main()
 {
-	cout << "Creating Window" << endl;
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 600, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-
-	SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-	SDL_RenderClear(renderer);
-
-	SDL_RenderPresent(renderer);
-	SDL_Delay(3000);
+	cout << "no" << endl;
+	renderEngine engine = renderEngine();
 	return 0;
 }
---
+++ Wing-Utils/Wing-Utils.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "Wing-Utils.h"
+
+renderEngine::renderEngine() {
+	std::cout << "Starting renderEngine" << std::endl;
+}
